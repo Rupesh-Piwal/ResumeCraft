252e8c0bf4d18d4294981fa43f4ae9c4abfb8617
rupesh <rpiwal02@gmail.com>
2024-03-30 15:47:31 +0530

    fixed home page alignment

--- client/src/components/Build.jsx
+++ client/src/components/Build.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Build = () => {
   return (
-    <div className="border  m-auto flex flex-col justify-center items-center gap-1">
+    <div className=" m-auto flex flex-col justify-center items-center gap-1 mt-[100px]">
       <h1 className="text-[40px]">Create Your Perfect Resume</h1>
       <h2 className="text-[20px] text-gray-500">
         Build a professional resume in minutes with our easy-to-use{" "}
         <span className="text-indigo-500 font-semibold">Resume Craft</span>
       </h2>
-      <button className="bg-indigo-600 text-white px-5 py-3 rounded-md text-xl font-semibold ">
+      <button
+        className="bg-indigo-600 hover:bg-indigo-500 text-white px-5 py-3 rounded-md text-xl font-semibold mt-5
+       "
+      >
         Craft My Resume
       </button>
     </div>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-indigo-600 py-6">
+    <footer className="bg-indigo-600 py-6 absolute bottom-0 w-full">
       <div className="container mx-auto text-center text-white">
         <p>&copy; 2024 Resume Generator. All rights reserved.</p>
       </div>
