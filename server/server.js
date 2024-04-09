738080a98999d0d5be2b31c1dd27dbdf51217775
rupesh <rpiwal02@gmail.com>
2024-04-09 10:44:50 +0530

    created user controller

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+const port = process.env.PORT || 5000;
 
+const app = express();
+
+app.get("/", (req, res) => res.send("hello mf"));
+
+app.listen(port, () => console.log(`server started on port:${port}`));
