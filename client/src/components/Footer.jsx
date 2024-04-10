01a3ed556347e3a0e951461723eb85e37cc9d3e9
rupesh <rpiwal02@gmail.com>
2024-04-10 14:02:10 +0530

    changes in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Get the current year dynamically
+
   return (
     <footer className="bg-indigo-700 py-6 absolute bottom-0 w-full">
       <div className="container mx-auto text-center text-white">
-        <p>&copy; 2024 Resume Craft. All rights reserved.</p>
+        <p>&copy; {currentYear} Resume Craft. All rights reserved.</p>
       </div>
     </footer>
   );
